a44bdb36755ccc4097aa3c5017eb827c95a52391
nanoeagle <fistrias.ikl@gmail.com>
2022-06-28 14:44:32 +0700

    Activity lifecycle: onCreate.

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -7,8 +7,62 @@ import android.widget.Button
 import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+    private val RUNNING_KEY = "running"
+    private val OFFSET_KEY = "offset"
+    private val BASE_KEY = "base"
+
+    private lateinit var stopwatch: Chronometer
+    private var running = false
+    private var offset: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        stopwatch = findViewById(R.id.stopwatch)
+
+        setListenersForButtons()
+    }
+
+    private fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    private fun setListenersForButtons() {
+        val startBtn = findViewById<Button>(R.id.start_btn)
+        startBtn.setOnClickListener { runStopwatchIfNotRunning() }
+
+        val pauseBtn = findViewById<Button>(R.id.pause_btn)
+        pauseBtn.setOnClickListener { pauseStopwatchIfRunning() }
+
+        val resetBtn = findViewById<Button>(R.id.reset_btn)
+        resetBtn.setOnClickListener { resetStopwatch() }
+    }
+
+    private fun runStopwatchIfNotRunning() {
+        if (!running) {
+            setBaseTime()
+            stopwatch.start()
+            running = true
+        }
+    }
+
+    private fun pauseStopwatchIfRunning() {
+        if (running) {
+            saveOffset()
+            stopwatch.stop()
+            running = false
+        }
+    }
+
+    private fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
+
+    private fun resetStopwatch() {
+        offset = 0
+        setBaseTime()
+        stopwatch.stop()
+        if (running) running = false;
     }
 }
